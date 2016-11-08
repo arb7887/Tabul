5657097f6f391bbfd5b64724baa2f3ef736c23c1
Ben Hoffman <bah8892@g.rit.edu>
2016-11-07 23:24:20 -0500

    Made a 3d test level
    I think that if we use some basic 3d shapes and then use the unity
    lighting system, physics and so forth that it would be a little better.
    I also think this will make our lives a little easier when it comes to
    things like adding forces and stuff too

---
+++ PlatformerThing/Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(PlayerPhysics))]
+[RequireComponent(typeof(Rigidbody))]
+
+public class PlayerInput : MonoBehaviour {
+
+    // Player character stuff
+    public float speed = 8f;
+    public float acceleration = 12f;
+
+    private float currentSpeed;
+    private float targetSpeed;
+    private Vector2 ammountToMove;
+
+    private PlayerPhysics playerPhysics;
+
+	// Use this for initialization
+	void Start ()
+    {
+        playerPhysics = GetComponent<PlayerPhysics>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        targetSpeed = Input.GetAxisRaw("Horizontal") * speed;
+        currentSpeed = IncrementTowards(currentSpeed, targetSpeed, acceleration);
+
+        ammountToMove = new Vector2(currentSpeed, 0f);
+        playerPhysics.Move(ammountToMove * Time.deltaTime);
+
+        if (Input.GetButton("Jump"))
+        {
+            playerPhysics.Jump();
+        }
+	}
+
+    private float IncrementTowards(float n, float target, float accel)
+    {
+        if(n == target)
+        {
+            return n;
+        }
+        else
+        {
+            float dir = Mathf.Sign(target - n);
+            n += accel * Time.deltaTime * dir;
+            return (dir == Mathf.Sign(target - n)) ? n : target;
+        }
+    }
+}
---
+++ PlatformerThing/Assets/Scripts/PlayerPhysics.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerPhysics : MonoBehaviour {
+
+    public float jumpHeight = 10f;
+    public float jumpTolerance = 0.5f;
+
+    private Rigidbody rb;
+    private RaycastHit hit;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    /// <summary>
+    /// To 
+    /// </summary>
+    /// <param name="moveAmount"></param>
+    public void Move(Vector2 moveAmount)
+    {
+        //transform.Translate(moveAmount);
+
+        Vector3 newPos = transform.position;
+        newPos.x += moveAmount.x;
+        newPos.y += moveAmount.y;
+
+        transform.position = newPos;
+
+    }
+
+    public void Jump()
+    {
+        // If I'm not in the air already, then jump
+        
+        if(Physics.Raycast(transform.position,Vector3.down,out hit, jumpTolerance))
+        {
+            rb.AddForce(Vector3.up * jumpHeight);
+        }
+    }
+}
